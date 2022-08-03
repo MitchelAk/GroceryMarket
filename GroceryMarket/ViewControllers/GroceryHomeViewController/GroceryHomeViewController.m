4e60c70e9e9d0c594eae73f9ee2a1e013695a506
MitchelAk <87458259+MitchelAk@users.noreply.github.com>
2022-08-03 16:10:59 -0700

    Add to cart mod

--- GroceryMarket/ViewControllers/GroceryHomeViewController/GroceryHomeViewController.m
+++ GroceryMarket/ViewControllers/GroceryHomeViewController/GroceryHomeViewController.m
@@ -169,11 +169,11 @@
     cell.groceryImage.image = [UIImage imageNamed:grocery.imageUrl];
     cell.groceryTitle.text = grocery.title;
     cell.groceryPrice.text = grocery.price;
-//    [[cell cartButton] addTarget:self action:@selector(clickEvent:event:) forControlEvents: UIControlEventTouchUpInside];
+    [[cell cartButton] addTarget:self action:@selector(clickEvent:event:) forControlEvents: UIControlEventTouchUpInside];
     return cell;
 }
 - (IBAction)clickEvent:(id)sender event:(id)event{
-    NSLog(@"You clicked fav: %@", event);
+    NSLog(@"You clicked fav");
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
