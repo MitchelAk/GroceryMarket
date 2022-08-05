98c320a44131a0bdaa21e5fb3c3effffb89349f4
MitchelAk <87458259+MitchelAk@users.noreply.github.com>
2022-08-05 15:10:06 -0700

    CartsViewController fix

--- GroceryMarket/ViewControllers/CartsViewController/CartsViewController.m
+++ GroceryMarket/ViewControllers/CartsViewController/CartsViewController.m
@@ -1,98 +1,137 @@
 //
-//  CartsViewController.m
+//  GroceryHomeViewController.m
 //  GroceryMarket
 //
-//  Created by Obinna Aguwa on 16/07/2022.
+//  Created by Obinna Aguwa on 13/07/2022.
 //
 
 #import "CartsViewController.h"
-#import "CartCollectionViewCell.h"
+#import "GroceryCollectionViewCell.h"
 #import "Grocery.h"
-
 @import FirebaseCore;
-@import FirebaseAuth;
 @import FirebaseFirestore;
+@import FirebaseAuth;
+
 
 @interface CartsViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout> {
-
-    __weak IBOutlet UICollectionView *cartCollectionView;
-
+    
+    
+    __weak IBOutlet UICollectionView *groceryCollectionView;
 }
-
-@property (nonatomic, strong) NSMutableArray<Grocery *> *cartList;
+@property (nonatomic, strong) NSMutableArray<Grocery *> *groceryList;
 
 @property (readwrite, nonatomic) FIRFirestore *db;
 
+
 @end
 
-@implementation CartsViewController
+@implementation GroceryHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-
+    
     self.db = [FIRFirestore firestore];
 
-    [self setUpCartList];
+    [self setUpGrcoryList];
 
-    cartCollectionView.delegate = self;
-    cartCollectionView.dataSource = self;
-
-    [cartCollectionView registerNib:[UINib nibWithNibName:@"CartCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"cellID"];
+    groceryCollectionView.delegate = self;
+    groceryCollectionView.dataSource = self;
+    
+    [groceryCollectionView registerNib:[UINib nibWithNibName:@"GroceryCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:@"cellID"];
 
 }
 
--(void) setUpCartList {
-    self.cartList = NSMutableArray.new;
+
+-(void) setUpGrcoryList {
+    self.groceryList = NSMutableArray.new;
+    
+    [[self.db collectionWithPath:@"products"] getDocumentsWithCompletion:^(FIRQuerySnapshot *snapshot, NSError *error) {
+        NSLog(@"Fetching docs");
+        if (error != nil) {
+            NSLog(@"Error getting documents: %@", error);
+        }else{
+            for (FIRDocumentSnapshot *document in snapshot.documents){
+//                NSLog(@"%@ => %@", document.documentID, document.data);
+                Grocery *grocery1 = Grocery.new;
+                grocery1.title = document.data[@"pname"];
+                grocery1.price = document.data[@"price"];
+                grocery1.imageUrl = @"image1";
+                
+                [self.groceryList addObject:grocery1];
+            }
+            groceryCollectionView.reloadData;
+        }
+    }];
+
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.groceryList.count;
+}
+
+- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    GroceryCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellID" forIndexPath:indexPath];
+    Grocery *grocery = self.groceryList[indexPath.row];
+    NSString *combPrice = [NSString stringWithFormat:@"%s%@", "$", grocery.price];
+    cell.groceryImage.image = [UIImage imageNamed:grocery.imageUrl];
+    cell.groceryTitle.text = grocery.title;
+    cell.groceryPrice.text = combPrice;
+    cell.cartButton.tag = indexPath.row;
+
+    [[cell cartButton] addTarget:self action:@selector(clickEvent:event:) forControlEvents: UIControlEventTouchUpInside];
+    
+    [[cell favoriteButton] addTarget:self action:@selector(likeEvent:withEvent:) forControlEvents:UIControlEventTouchDownRepeat];
+    
+    return cell;
+}
+
+- (IBAction)likeEvent:(id)sender withEvent:(UIEvent*)event {
+
+    UITouch* touch = [[event allTouches] anyObject];
+    CGPoint currenTouchPosition = [touch locationInView:groceryCollectionView];
+    NSIndexPath *indexPath = [groceryCollectionView indexPathForItemAtPoint:currenTouchPosition];
+    
+    GroceryCollectionViewCell *cell = [groceryCollectionView dequeueReusableCellWithReuseIdentifier:@"cellID" forIndexPath:indexPath];
+
+    if (touch.tapCount == 2) {
+        NSLog(@"YOU TAPPED TWICE");
+            [cell.favoriteButton setImage:[UIImage imageNamed:@"favorite-16"] forState:UIControlStateNormal];
+    }
+}
+
+- (IBAction)clickEvent:(id)sender event:(id)event{
+    NSSet *touches = [event allTouches];
+    
+    UITouch *touch = [touches anyObject];
+    
+    CGPoint currenTouchPosition = [touch locationInView:groceryCollectionView];
+    
+    NSIndexPath *indexPath = [groceryCollectionView indexPathForItemAtPoint:currenTouchPosition];
+    
+
+    Grocery *gg = self.groceryList[indexPath.row];
     
     FIRUser *user = [FIRAuth auth].currentUser;
     NSString *uid = user.uid;
 
-    
-    [[[[self.db collectionWithPath:@"users"] documentWithPath:uid] collectionWithPath:@"mycart"] getDocumentsWithCompletion:^(FIRQuerySnapshot *snapshot, NSError *error) {
-            NSLog(@"Fetching cart docs");
-            if (error != nil) {
-                NSLog(@"Error getting documents: %@", error);
-            }else{
-                for (FIRDocumentSnapshot *document in snapshot.documents){
-                    NSLog(@"%@ => %@", document.documentID, document.data);
-                    Grocery *grocery1 = Grocery.new;
-                    grocery1.title = document.data[@"pname"];
-                    grocery1.price = document.data[@"price"];
-                    grocery1.imageUrl = @"image1";
-                    
-//                    [self.cartList addObject:grocery1];
-                }
-//                cartCollectionView.reloadData;
-            }
+    if (user) {
+    [[[[self.db collectionWithPath:@"users"]
+      documentWithPath: uid] collectionWithPath:@"mycart"]
+      addDocumentWithData:@{
+        @"pname":gg.title,
+        @"price":gg.price,
+        @"image":gg.imageUrl
+      } completion:^(NSError *  _Nullable error){
+        if(error != nil){
+            NSLog(@"Error adding document: %@", [error localizedDescription]);
+        } else{
+            NSLog(@"Added product to your cart list: %@", uid);
+        }
     }];
+        
+    }
     
-    Grocery *grocery1 = Grocery.new;
-        grocery1.title = @"Gatorade Frost Thirst Quencher Sports Drink";
-        grocery1.price = @"$6.98";
-    grocery1.imageUrl = @"image1";
-
-    [self.cartList addObject:grocery1];
-
-
-}
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.cartList.count;
-}
-
-- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    CartCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cellID" forIndexPath:indexPath];
-    
-    Grocery *grocery = self.cartList[indexPath.row];
-    
-    NSString *combPrice = [NSString stringWithFormat:@"%s%@", "$", grocery.price];
-    
-    cell.groceryImage.image = [UIImage imageNamed:grocery.imageUrl];
-    cell.groceryName.text = grocery.title;
-    cell.groceryPrice.text = combPrice;
-
-    return cell;
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -102,5 +141,8 @@
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
     return UIEdgeInsetsMake(10, 5, 10, 5);
 }
+
+
+
 
 @end
