27550fe784c944d17eac61b89fe09673b0529585
Mitchel <mitcheligolimah@gmail.com>
2022-08-04 19:46:59 -0700

    Created a CartCollectionViewCell

---
+++ GroceryMarket/ViewControllers/CartsViewController/CartCollectionViewCell.h
@@ -0,0 +1,16 @@
+//
+//  CartCollectionViewCell.h
+//  GroceryMarket
+//
+//  Created by Mitchel Igolimah on 8/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CartCollectionViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
