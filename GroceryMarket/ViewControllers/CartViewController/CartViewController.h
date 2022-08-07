5128e92702c53975902ce51974e6d84460b62bae
MitchelAk <87458259+MitchelAk@users.noreply.github.com>
2022-08-07 09:58:32 -0700

    Cart Collection

--- GroceryMarket/ViewControllers/CartViewController/CartViewController.h
+++ GroceryMarket/ViewControllers/CartViewController/CartViewController.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CartViewController : UIViewController
+@interface CartViewController : UIViewController<UICollectionViewDelegate, UICollectionViewDataSource>
 
 @end
 
