dd289730363ab10a535f2b88de5c9c8bb67d0ce4
MitchelAk <87458259+MitchelAk@users.noreply.github.com>
2022-08-09 09:50:03 -0700

    Update Search

--- GroceryMarket/ViewControllers/SearchViewController/SearchViewController.m
+++ GroceryMarket/ViewControllers/SearchViewController/SearchViewController.m
@@ -32,15 +32,12 @@
             for (FIRDocumentSnapshot *document in snapshot.documents){
                 NSLog(@"Search Products: %@", document.data[@"pname"]);
 
-                self->groceryArray = [[NSArray alloc] initWithArray:document.data[@"pname"]];
+//                self->groceryArray = [[NSArray alloc] initWithArray:document.data[@"pname"]];
                 
                 
-                self->displayGrocery = [[NSMutableArray alloc]initWithArray:self->groceryArray];
+                self->displayGrocery = [[NSMutableArray alloc]initWithArray:document.data[@"pname"]];
 
             }
-            
-            [self->displayGrocery addObject:self->groceryArray];
-
 
         }
     }];
